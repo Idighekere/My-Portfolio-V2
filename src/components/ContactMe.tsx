88137a72a6b541eb0eb80444fedad70cc7629c70
Idighekere Udo <idighekereudo@gmail.com>
2024-08-27 01:44:07 +0100

    fix: input form color constrast reducd

--- src/components/ContactMe.tsx
+++ src/components/ContactMe.tsx
@@ -9,11 +9,11 @@ const ContactMe = (props: Props) => {
 
             <form action="">
                 <div className="flex flex-col gap-3 w-full  md:items-center">
-                    <input type="text" name="" id="" className="bg-transparent border border-primary-blue rounded-md px-2 py-1 w-full marker:md:w-64 lg:w-96 placeholder:text-soft-white" placeholder="Full Name" required />
-                    <input type="mail" name="" id="" className="bg-transparent border border-primary-blue rounded-md px-2 py-1 w-full marker:md:w-64 lg:w-96 placeholder:text-soft-white focus:text-soft-white focus:border-text-soft-white" placeholder="Email Address" required />
-                    <input type="text" name="" id="" className="bg-transparent border border-primary-blue rounded-md px-2 py-1 w-full marker:md:w-64 lg:w-96 placeholder:text-soft-white focus:text-soft-white focus:border-text-soft-white" placeholder="Phone Number" />
-                    <textarea name="" id="" cols="30" rows="10" className="bg-transparent border border-primary-blue rounded-md px-2 py-1 w-full md:w-64 lg:w-96 placeholder:text-soft-white focus:text-soft-white focus:border-text-soft-white" placeholder="Message"></textarea>
-                    <button className="border border-primary-blue bg-primary-blue px-3 py-1 text-soft-white w-full md:w-64 rounded-md lg:w-96 duration-200 hover:bg-transparent hover:border" >Send Message</button>
+                    <input type="text" name="" id="" className="bg-transparent border border-primary-blue/80 rounded-md px-2 py-1 w-full marker:md:w-64 lg:w-96 placeholder:text-soft-white/50" placeholder="Full Name" required />
+                    <input type="mail" name="" id="" className="bg-transparent border border-primary-blue/80 rounded-md px-2 py-1 w-full marker:md:w-64 lg:w-96 placeholder:text-soft-white/50 focus:text-soft-white/50 focus:border-text-soft-white/50" placeholder="Email Address" required />
+                    <input type="text" name="" id="" className="bg-transparent border border-primary-blue/80 rounded-md px-2 py-1 w-full marker:md:w-64 lg:w-96 placeholder:text-soft-white/50 focus:text-soft-white/50 focus:border-text-soft-white/50" placeholder="Phone Number" />
+                    <textarea name="message" id="message" cols={"20"} rows={"10"} className="bg-transparent border border-primary-blue/80 rounded-md px-2 py-1 w-full md:w-64 lg:w-96 placeholder:text-soft-white/50 focus:text-soft-white/50 focus:border-text-soft-white/50" placeholder="Message"></textarea>
+                    <button className="border border-primary-blue/80 bg-primary-blue/80 px-3 py-1 text-soft-white/50 w-full md:w-64 rounded-md lg:w-96 duration-200 hover:bg-transparent hover:border" >Send Message</button>
                 </div>
             </form >
 
